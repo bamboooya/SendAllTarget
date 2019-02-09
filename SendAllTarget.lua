6f8d025420f11465cd7eecb783bcc11682ebe1f1
KalesAndRancor <some___guy@hotmail.com>
2019-02-08 20:43:23 -0600

    Ability to send commands to specific boxes added, command syntax changed.

--- SendAllTarget.lua
+++ SendAllTarget.lua
@@ -6,13 +6,16 @@ _addon.version = '1.0'
 _addon.author = 'Selindrile, Thanks and apologies to Arcon for abusing his code.'
 _addon.commands = {'sendalltarget','sendallt','sendat','sat'}
 
-windower.register_event('addon command',function (cmd,...)
+windower.register_event('addon command',function (cmd,cmd2,...)
 	if cmd == nil then return
 	elseif cmd == 'alltarget' then
 		local target = windower.ffxi.get_mob_by_target('t')
 		windower.send_command('send @all sendalltarget target ' .. tostring(target.id))
+	elseif cmd == 'youtarget' then
+		local target = windower.ffxi.get_mob_by_target('t')
+		windower.send_command('send '..cmd2..' sendalltarget target ' .. tostring(target.id))
 	elseif cmd == 'target' then
-		local id = tonumber(...)
+		local id = tonumber(cmd2)
 		local target = windower.ffxi.get_mob_by_id(id)
 		if not target then
 			return
@@ -24,11 +27,21 @@ windower.register_event('addon command',function (cmd,...)
 			['Target'] = target.id,
 			['Player Index'] = player.index,
 		}))
-	elseif cmd == 'command' then
+	elseif cmd == 'allcommand' then
 		local command = ...
 		local mobid = windower.ffxi.get_mob_by_target('t')
 		if mobid and mobid.id then
-			windower.send_command('send @all '..command..' '..mobid.id..'')
+			if command == nil then
+				windower.send_command('send @all '..cmd2..' '..mobid.id..'')
+			elseif cmd2 then
+				windower.send_command('send @all '..cmd2..' '..command..' '..mobid.id..'')
+			end
+		end
+	elseif cmd == 'youcommand' then
+		local command = ...
+		local mobid = windower.ffxi.get_mob_by_target('t')
+		if mobid and mobid.id then
+			windower.send_command('send '..cmd2..' '..command..' '..mobid.id..'')
 		end
     end
 end)
